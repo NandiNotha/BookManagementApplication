665e01ea861091dbc7a8fe3d21374ece5ede69da
NandiNxumalo <nandi@digitalsolutionfoundry.co.za>
2024-06-20 16:01:13 +0200

    home page updates

--- book-management-frontend/src/components/Home.js
+++ book-management-frontend/src/components/Home.js
@@ -4,11 +4,10 @@ const Home = () => {
   return (
     <div>
       <header>
-        <h1>My Simple React Home Page</h1>
+        <h1>Book Management</h1>
       </header>
       <main>
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
-        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+        <p>Welcome to Book Management.</p>
       </main>
     </div>
   );
